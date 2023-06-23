c361b9d5f44efe4ec9849a66bd4f8c5c7734d0fc
toluwanithepm <johndoe@example.com>
2023-06-23 01:49:30 +0100

    Import Dependencies and Initialize Express App

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-// app.js
 const express = require('express');
+
+// Create an instance of the Express application
 const app = express();
+
+// Define routes and middleware here
+
+// Start the server
 const port = 3000;
-
-app.set('view engine', 'ejs');
-
-// Add routes and middleware here
-
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
